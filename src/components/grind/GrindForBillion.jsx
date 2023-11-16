5e51ed43731879a090ced96643ee244f6af9f69f
Zentey <zenteymc@gmail.com>
2023-11-16 01:45:20 -0300

    Se modificaron las seccionas GrindForHour y GrindForBillion para que funcionen

--- src/components/grind/GrindForBillion.jsx
+++ src/components/grind/GrindForBillion.jsx
@@ -1,26 +1,89 @@
 import React, { useState } from 'react';
-import { Card, Typography, Slider, Button } from 'antd';
+import { Card, Typography, Slider, Button, Input } from 'antd';
+import Swal from 'sweetalert2';
+import { collection, addDoc, getFirestore } from 'firebase/firestore';
+import { db } from '../../firebase/client';
 
 const { Paragraph } = Typography;
 
 const GrindForBillion = ({ hourlyRate }) => {
-    const [selectedBillions, setSelectedBillions] = useState(1)
+    const [selectedBillions, setSelectedBillions] = useState(1);
+    const [user, setUser] = useState({
+        name: '',
+        email: '',
+        password: '',
+    });
 
     const handleSliderChange = (value) => {
-        setSelectedBillions(Math.round(value))
-    };
-
-    const handleContractClick = () => {
-        console.log(`We will grind for you ${selectedBillions} hours.`);
+        setSelectedBillions(Math.round(value));
     };
 
     const calculateTotalCost = () => {
         return hourlyRate * selectedBillions;
     };
 
+    const handleInputChange = (e) => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleContractClick = () => {
+        const orderData = {
+            buyer: user.name,
+            email: user.email,
+            password: user.password,
+            billions: selectedBillions,
+            price: calculateTotalCost(),
+        };
+
+        const db = getFirestore();
+        const billionsCollection = collection(db, "billions");
+
+        addDoc(billionsCollection, orderData)
+            .then(({ id }) =>
+                Swal.fire({
+                    title: 'Success!',
+                    text: `We will grind for you ${selectedBillions} billion raw silver on the provided account.
+                        Your grind ID is: ${id}`,
+                    icon: 'success',
+                    confirmButtonText: 'Cool',
+                })
+            )
+            .catch((error) =>
+                Swal.fire({
+                    title: 'Error!',
+                    text: `Something bad happened: ${error}`,
+                    icon: 'error',
+                    confirmButtonText: 'Ok',
+                })
+            );
+    };
+
     return (
         <Card title="Grind for Billion" style={{ width: 300, margin: '20px' }}>
             <Paragraph>Price per billion: ${hourlyRate}</Paragraph>
+            <Input
+                placeholder="Your name"
+                name="name"
+                value={user.name}
+                onChange={handleInputChange}
+            />
+            <Input
+                placeholder="Email (BDO Account)"
+                name="email"
+                type="email"
+                value={user.email}
+                onChange={handleInputChange}
+            />
+            <Input
+                placeholder="Password (BDO Account)"
+                name="password"
+                type="password"
+                value={user.password}
+                onChange={handleInputChange}
+            />
             <Paragraph>Select the quantity of billions:</Paragraph>
             <Slider
                 min={1}
@@ -28,14 +91,15 @@ const GrindForBillion = ({ hourlyRate }) => {
                 step={1}
                 marks={{ 1: '1', 50: '200' }}
                 onChange={handleSliderChange}
-                value={selectedBillions}/>
+                value={selectedBillions}
+            />
             <Paragraph>We will grind on your account {selectedBillions} billions</Paragraph>
             <Paragraph>Total price: ${calculateTotalCost()}</Paragraph>
             <Button type="primary" onClick={handleContractClick}>
-                Hire us
+                Place order
             </Button>
         </Card>
     );
-}
+};
 
 export default GrindForBillion;
--- src/components/grind/GrindForHour.jsx
+++ src/components/grind/GrindForHour.jsx
@@ -1,41 +1,105 @@
 import React, { useState } from 'react';
-import { Card, Typography, Slider, Button } from 'antd';
+import { Card, Typography, Slider, Button, Input } from 'antd';
+import Swal from 'sweetalert2';
+import { collection, addDoc, getFirestore } from 'firebase/firestore';
+import { db } from '../../firebase/client';
 
 const { Paragraph } = Typography;
 
 const GrindForHour = ({ hourlyRate }) => {
-    const [selectedHours, setSelectedHours] = useState(1)
+    const [selectedHours, setSelectedBillions] = useState(1);
+    const [user, setUser] = useState({
+        name: '',
+        email: '',
+        password: '',
+    });
 
     const handleSliderChange = (value) => {
-        setSelectedHours(Math.round(value))
-    };
-
-    const handleContractClick = () => {
-        console.log(`We will grind for you ${selectedHours} hours.`);
+        setSelectedBillions(Math.round(value));
     };
 
     const calculateTotalCost = () => {
         return hourlyRate * selectedHours;
     };
 
+    const handleInputChange = (e) => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleContractClick = () => {
+        const orderData = {
+            buyer: user.name,
+            email: user.email,
+            password: user.password,
+            hours: selectedHours,
+            price: calculateTotalCost(),
+        };
+
+        const db = getFirestore();
+        const hoursCollection = collection(db, "hours");
+
+        addDoc(hoursCollection, orderData)
+            .then(({ id }) =>
+                Swal.fire({
+                    title: 'Success!',
+                    text: `We will grind for you ${selectedHours} hours on the provided account.
+                        Your grind ID is: ${id}`,
+                    icon: 'success',
+                    confirmButtonText: 'Cool',
+                })
+            )
+            .catch((error) =>
+                Swal.fire({
+                    title: 'Error!',
+                    text: `Something bad happened: ${error}`,
+                    icon: 'error',
+                    confirmButtonText: 'Ok',
+                })
+            );
+    };
+
     return (
         <Card title="Grind for Hour" style={{ width: 300, margin: '20px' }}>
             <Paragraph>Price per hour: ${hourlyRate}</Paragraph>
+            <Input
+                placeholder="Your name"
+                name="name"
+                value={user.name}
+                onChange={handleInputChange}
+            />
+            <Input
+                placeholder="Email (BDO Account)"
+                name="email"
+                type="email"
+                value={user.email}
+                onChange={handleInputChange}
+            />
+            <Input
+                placeholder="Password (BDO Account)"
+                name="password"
+                type="password"
+                value={user.password}
+                onChange={handleInputChange}
+            />
             <Paragraph>Select the quantity of hours:</Paragraph>
             <Slider
                 min={1}
                 max={50}
                 step={1}
-                marks={{ 1: '1', 50: '50' }}
+                marks={{ 1: '1', 50: '200' }}
                 onChange={handleSliderChange}
-                value={selectedHours}/>
+                value={selectedHours}
+            />
             <Paragraph>We will grind on your account {selectedHours} hours</Paragraph>
             <Paragraph>Total price: ${calculateTotalCost()}</Paragraph>
             <Button type="primary" onClick={handleContractClick}>
-                Hire us
+                Place order
             </Button>
         </Card>
     );
-}
+};
 
 export default GrindForHour;
